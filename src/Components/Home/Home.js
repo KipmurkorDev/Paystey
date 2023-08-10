2e9149ba355efe009922aca8513529a7b2ddd083
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2023-08-10 15:58:57 +0300

    .

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -87,7 +87,7 @@ function Home() {
                             We love all of our beautiful brands here at Paystey but there is a special place in our heart for the Austrilia brands.
                             We have made it easy for you to find a little about each of these amazing brands.
                         </p>
-                        <button className="btn btn-dark pmb-5">Find Out More</button>
+                        <button className="btn btn-dark mb-5">Find Out More</button>
                     </Col>
                     <Col className='w-75' style={{ paddingTop: "10%" }}>
                         <img src={HomeGrown} alt='home-grown' className='home-grown-img' />
